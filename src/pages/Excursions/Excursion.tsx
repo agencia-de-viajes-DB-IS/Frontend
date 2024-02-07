7b1fff5453fe71b19cd2f083219fb635e2c3a6cc
Alfredo Nuno <anuno61044@gmail.com>
2024-02-06 23:38:45 -0500

    cleaning components

--- src/pages/Excursions/Excursion.tsx
+++ src/pages/Excursions/Excursion.tsx
@@ -3,6 +3,7 @@ import { Header } from "../../components/Header/Header";
 import { Navbar } from "../../components/Navbar/Navbar";
 import './styles.css'
 import { Card } from "../../components/Card/Card";
+import { Searchbar } from "../../components/Search/Searchbar";
 
 
 export function Excursions() {
@@ -17,27 +18,7 @@ export function Excursions() {
                 <Navbar/>
 
                 <div className="excursion-section">
-                    <div className="search-section">
-                        <div className="search-from">
-                            <label htmlFor="">Desde</label>
-                            <input type="text" placeholder="Escribe aquí ..."/>
-                        </div>
-                        <div className="search-to">
-                            <label htmlFor="">Hasta</label>
-                            <input type="text" placeholder="Escribe aquí ..."/>
-                        </div>
-                        <div className="search-to">
-                            <label htmlFor="">Precio Mínimo</label>
-                            <input type="text" placeholder="Escribe aquí ..."/>
-                        </div>
-                        <div className="search-to">
-                            <label htmlFor="">Precio Máximo</label>
-                            <input type="text" placeholder="Escribe aquí ..."/>
-                        </div>
-                        <div className="search-btn">
-                            <button type="button" onClick={alert}>Buscar</button>
-                        </div>
-                    </div>
+                    <Searchbar/>
 
                     <div className="excursion-container">
                         {cards.map((_, index) => (
